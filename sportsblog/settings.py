273e48f3e20a321c24d687cbc30baa186f8c2a52
Saktheeswaran7103 <saktheeswaran7103@gmail.com>
2025-10-07 22:45:47 +0530

    commoit changes

--- sportsblog/settings.py
+++ sportsblog/settings.py
@@ -131,10 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = [BASE_DIR / 'media']
-CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
