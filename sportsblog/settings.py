a7cb2784c03538abb475fd74aa42219e26c9e6df
Saktheeswaran7103 <saktheeswaran7103@gmail.com>
2025-10-07 23:09:23 +0530

    allowed host changed

--- sportsblog/settings.py
+++ sportsblog/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get("DEFAULT","False").lower == ("true")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost").split(" ")
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
