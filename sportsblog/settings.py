9be617b1dca4df0dc829c808e63122bc3bfc2b52
Saktheeswaran7103 <saktheeswaran7103@gmail.com>
2025-10-07 23:28:57 +0530

    allowed host changed

--- sportsblog/settings.py
+++ sportsblog/settings.py
@@ -132,10 +132,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = [BASE_DIR / 'media']
-CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
+#CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
+print("CLOUDINARY_URL:", os.getenv("CLOUDINARY_URL"))
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
